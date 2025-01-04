d6c0d22eb00286206ff8cb8027fb59f2ae0deee0
badr32425 <badr32425@gmail.com>
2025-01-04 01:31:37 +0100

    solving dashboard and adding code and marque

--- data/import_dev_data.js
+++ data/import_dev_data.js
@@ -1,40 +1,80 @@
-import { connect } from "mongoose";
-import { readFileSync } from "fs";
-require("dotenv").config({ path: "../config.env" });
-import { create, deleteMany } from "../model/productModel";
-
-const products = JSON.parse(readFileSync("products_example.json", "utf-8"));
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../config.env' });
+import sqlite3 from 'sqlite3';
+import User from '../model/userModel.js';
 
 connect(process.env.MONGO_URI)
-  .then((response) => {
-    console.log(`MongDB Connected : ${response.connection.host}`);
-  })
-  .catch((error) => {
-    console.log("Error in DB connection: " + error);
-  });
+    .then((response) => {
+        console.log(`MongDB Connected : ${response.connection.host}`);
+    })
+    .catch((error) => {
+        console.log('Error in DB connection: ' + error);
+    });
 
-const importData = async () => {
-  try {
-    await create(products);
-    console.log("Data uploaded successfully !");
-    process.exit(1);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// Enable verbose mode for debugging
+sqlite3.verbose();
 
-const deleteAllData = async () => {
-  try {
-    await deleteMany();
-    console.log("Data Deleted successfully !");
-    process.exit(1);
-  } catch (err) {
-    console.log(err);
-  }
-};
+const dbPath = './M_DataBase.db';
 
-if (process.argv[2] == "--import") {
-  importData();
-} else if (process.argv[2] == "--delete") {
-  deleteAllData();
-}
+// Connect to the database
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+});
+
+const query = 'SELECT * FROM Produits';
+
+// Execute the query
+db.all(query, [], async (err, rows) => {
+    if (err) {
+        console.error('Error executing query:', err.message);
+    } else {
+        const suggestions = rows.map((row) => row.designation);
+        console.log(suggestions);
+        const targetUser = await User.findById('6777f6238c5d83b0c87f0134');
+        console.log(targetUser);
+        targetUser.suggestionsList = suggestions;
+        await targetUser.save({ validateBeforeSave: false });
+    }
+
+    // Close the database connection
+    db.close((err) => {
+        if (err) {
+            console.error('Error closing the database:', err.message);
+        } else {
+            console.log('Database connection closed.');
+        }
+    });
+});
+
+// const products = JSON.parse(readFileSync('products_example.json', 'utf-8'));
+
+// const importData = async () => {
+//     try {
+//         await create(products);
+//         console.log('Data uploaded successfully !');
+//         process.exit(1);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+
+// const deleteAllData = async () => {
+//     try {
+//         await deleteMany();
+//         console.log('Data Deleted successfully !');
+//         process.exit(1);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+
+// if (process.argv[2] == '--import') {
+//     importData();
+// } else if (process.argv[2] == '--delete') {
+//     deleteAllData();
+// }
